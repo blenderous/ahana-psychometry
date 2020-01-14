60bb3e1a958574f539e62f25ab5a5a991cd6e245
blenderous <hpone.n.only@gmail.com>
2020-01-14 16:56:45 +0530

    Updates

---
+++ precache-manifest.3417e0c70eae5f9f7149d384d1a777da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef6410addc97c3b63252fd2d566f4fa",
+    "url": "/ahana-psychometry/index.html"
+  },
+  {
+    "revision": "9f29ce6cff5af036c2b0",
+    "url": "/ahana-psychometry/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "33d75d7974d4d1f7eb05",
+    "url": "/ahana-psychometry/static/css/main.a883f588.chunk.css"
+  },
+  {
+    "revision": "9f29ce6cff5af036c2b0",
+    "url": "/ahana-psychometry/static/js/2.18ef6a04.chunk.js"
+  },
+  {
+    "revision": "33d75d7974d4d1f7eb05",
+    "url": "/ahana-psychometry/static/js/main.36a11fdd.chunk.js"
+  },
+  {
+    "revision": "2dedeff07363e90ebf7c",
+    "url": "/ahana-psychometry/static/js/runtime-main.b3c51291.js"
+  }
+]);
