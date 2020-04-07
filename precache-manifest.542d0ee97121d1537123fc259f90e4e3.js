212e93c4ed034d387fa96a00df3aa50187c4567a
blenderous <hpone.n.only@gmail.com>
2020-04-07 16:20:17 +0530

    Updates

---
+++ precache-manifest.542d0ee97121d1537123fc259f90e4e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5155b226bed588b7f46498035c5d2329",
+    "url": "/ahana-psychometry/index.html"
+  },
+  {
+    "revision": "ca8186085769eb0c881e",
+    "url": "/ahana-psychometry/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "81ad46b49f4e13cb6571",
+    "url": "/ahana-psychometry/static/css/main.650a70f7.chunk.css"
+  },
+  {
+    "revision": "ca8186085769eb0c881e",
+    "url": "/ahana-psychometry/static/js/2.e195d3c1.chunk.js"
+  },
+  {
+    "revision": "81ad46b49f4e13cb6571",
+    "url": "/ahana-psychometry/static/js/main.ff582a81.chunk.js"
+  },
+  {
+    "revision": "2dedeff07363e90ebf7c",
+    "url": "/ahana-psychometry/static/js/runtime-main.b3c51291.js"
+  },
+  {
+    "revision": "ed4bc9989a3472ce95b125764627a3d5",
+    "url": "/ahana-psychometry/static/media/logo-ahana.ed4bc998.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ahana-psychometry/precache-manifest.344fc57e16e265cdf1ea1bb791a32a8b.js"
+  "/ahana-psychometry/precache-manifest.542d0ee97121d1537123fc259f90e4e3.js"
 );
 
 self.addEventListener('message', (event) => {
